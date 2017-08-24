fc7b91bcd73ccfc7c94ffa61bfb081df677cdc82
Justin Sobota <jusitn.sobota0227@gmail.com>
2017-08-24 03:08:49 -0400

    index signUp and dbh added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<form action="signUp.php" method="POST">
+		<input type="text" name="first" placeholder="First Name"><br>
+		<input type="text" name="last" placeholder="Last Name"><br>
+		<input type="text" name="uid" placeholder="Username"><br>
+		<input type="password" name="pwd" placeholder="Password"><br>
+		<button type="submit">Sign Up</button><br>
+	</form>
+
+</body>
+</html>
